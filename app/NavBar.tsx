3f16034c11c2392cca06dfb3df3657a09f6f8348
aman-dev02 <145318136+aman-dev02@users.noreply.github.com>
2023-10-16 17:05:26 +0530

    ACTIVE LINK NAV

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,19 +1,38 @@
-import Link from 'next/link'
-import React from 'react'
-import {AiFillBug} from 'react-icons/ai'
+"use client";
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 const NavBar = () => {
-    const links=[
-        {label:'Dashboard',href:'/'},
-        {label:'Issues',href:'/issues'}
-    ]
+  const currentpath = usePathname();
+  console.log(currentpath);
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href="/"><AiFillBug/></Link>
-      <ul className='flex space-x-6'>
-      {links.map(link=> <Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>)}
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/" className="text-[#245953]">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+              "text-zinc-900": link.href == currentpath,
+              "text-zinc-500": link.href !== currentpath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
