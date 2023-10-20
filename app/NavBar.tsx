1772fd31bb7c35fcb245be31cf89bccced0ec735
aman-dev02 <145318136+aman-dev02@users.noreply.github.com>
2023-10-20 10:44:50 +0530

    nav changes

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,20 +1,20 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { AiFillBug } from "react-icons/ai";
+import { DiTerminalBadge } from "react-icons/di";
 import { usePathname } from "next/navigation";
 import classnames from "classnames";
 const NavBar = () => {
   const currentpath = usePathname();
-  console.log(currentpath);
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/" className="text-[#245953]">
-        <AiFillBug />
+      <Link href="/" className=" text-4xl">
+        <DiTerminalBadge />
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
